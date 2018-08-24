747afa622e84e29f673746bcc82becf969f50290
DavidPerezAparicio <david.osirisvenus.venus90@gmail.com>
2018-08-24 11:12:01 -0500

    scanf y printf

--- LOVE.c
+++ LOVE.c
@@ -4,8 +4,12 @@ int main()
 {
 	printf("Ordentamiento de burbuja");
 	int Numeros[10];
+	int i;
+	for ( i=0;i<10;i++)
+	   scanf("%d", &Numeros[i]);
 	
-	
-	for (int i=0;i)
+	for(i=0;i<10;i++)
+	    printf("Numero en Numeros[%d] es %d", i, Numeros[i]);
+	//for (int i=0;i)
 	return 0;
 }
